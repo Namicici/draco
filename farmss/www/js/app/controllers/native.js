c4cb83b287e4af5076745ad8e1a55d540906d33e
Sissy Li <847954654@qq.com>
2015-03-19 20:01:01 +0800

    add provinces and cities in China. add fs-locate directives (not yet finished)

--- farmss/www/js/app/controllers/native.js
+++ farmss/www/js/app/controllers/native.js
@@ -1,20 +1,52 @@
 angular.module('farmss.controllers')
 
-.controller('NativeCtrl', function($scope) {
+.controller('NativeCtrl', function($scope){
     $scope.places = ["坝陵", "半月", "河溶"];
     $scope.types = ["割谷", "耕田", "喷药"];
     $scope.sorts = ["距离最近", "价格最低"];
-
     $scope.items = [1,2,3];
-    $scope.doRefresh = function() {
-        $http.get('/new-items')
-         .success(function(newItems) {
-           $scope.items = newItems;
-         })
-         .finally(function() {
-           // Stop the ion-refresher from spinning
-           $scope.$broadcast('scroll.refreshComplete');
-         });
-     };
 
+    $scope.serviceScope = [
+        {
+            province: "湖北",
+            regions:[
+                {
+                    region: "宜昌",
+                    countries:[
+                        {
+                            country: "当阳",
+                            towns:[{town: "坝陵办事处", villages:["黄林村", "苏河村"]}]
+                        },{
+                            country: "宜都",
+                            towns:[]
+                        },{
+                            country: "枝江",
+                            towns:[]
+                        },{
+                            country: "远安",
+                            towns:[]
+                        },{
+                            country: "兴山",
+                            towns:[]
+                        },{
+                            country: "秭归",
+                            towns:[]
+                        }
+                    ]
+
+                }
+            ]
+        }
+    ];
+
+    $scope.doRefresh = function(){
+        $http.get('/new-items')
+            .success(function(newItems){
+                $scope.items = newItems;
+            })
+            .finally(function(){
+                // Stop the ion-refresher from spinning
+                $scope.$broadcast('scroll.refreshComplete');
+        });
+    };
 })
---
+++ farmss/www/js/app/directives/locate.js
@@ -0,0 +1,38 @@
+angular.module('farmss.directives')
+
+.directive('fsLocate', function(){
+    return {
+        restrict: "EA",
+        transclude: true,
+        scope:{
+            items:"="
+        },
+        templateUrl: "./templates/directives/locate.html",
+
+        controller: function($scope, $ionicModal){
+            $scope.selected = "当阳";
+
+            initModal = function(){
+                $ionicModal.fromTemplateUrl("./templates/directives/locationsModal.html", {
+                    scope: $scope,
+                    animation: 'slide-in-up'
+                }).then(function(modal){
+                    $scope.modal = modal;
+                });
+            };
+
+            initModal();
+
+            $scope.changeLocation = function(){
+                if ($scope.modal)
+                    $scope.openModal = $scope.modal.show();
+            };
+
+            $scope.select = function(item){
+                $scope.selected = item;
+                $scope.modal.hide();
+            };
+        }
+    };
+
+});
